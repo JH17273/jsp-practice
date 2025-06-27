9833551c21518c8a265eb0ebe9db78277febc0ac
DESKTOP-S6H8PKS\user1 <hanel9900906@gmail.com>
2025-06-27 17:54:03 +0900

    jsp실습

--- src/main/java/com/kh/practice/MemberSearchServlet.java
+++ src/main/java/com/kh/practice/MemberSearchServlet.java
@@ -25,21 +25,32 @@ public class MemberSearchServlet extends HttpServlet {
 	public void init() {
 		// MOCK 회원 데이터
 		mockList.add(new Member("홍은동", 25, "남자"));
-        mockList.add(new Member("김영심", 19, "여자"));
-        mockList.add(new Member("이은수", 30, "남자"));
-        mockList.add(new Member("심하늘", 22, "여자"));
-        mockList.add(new Member("정심훈", 45, "남자"));
-        mockList.add(new Member("심은성", 4, "남자"));
-        mockList.add(new Member("나수지", 33, "여자"));
-        mockList.add(new Member("유상성", 21, "남자"));
-        mockList.add(new Member("한지은", 28, "여자"));
-        mockList.add(new Member("고심현", 40, "남자"));
+		mockList.add(new Member("김영심", 19, "여자"));
+		mockList.add(new Member("이은수", 30, "남자"));
+		mockList.add(new Member("심하늘", 22, "여자"));
+		mockList.add(new Member("정심훈", 45, "남자"));
+		mockList.add(new Member("심은성", 4, "남자"));
+		mockList.add(new Member("나수지", 33, "여자"));
+		mockList.add(new Member("유상성", 21, "남자"));
+		mockList.add(new Member("한지은", 28, "여자"));
+		mockList.add(new Member("고심현", 40, "남자"));
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 클라이언트에서 전달한 이름이 포함된 회원을 mockList에서 조회한 후 새로운
-		// List에 담은 후 , 적절한 scope에 담아 result.jsp로 응답처리 위임할 것. 
+		// List에 담은 후 , 적절한 scope에 담아 result.jsp로 응답처리 위임할 것.
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		List<Member> resultList = new ArrayList<>();
+		for (Member m : mockList) {
+			if (m.getName().contains(name)) {
+				resultList.add(m);
+			}
+		}
+		request.setAttribute("resultList", resultList);
+
+		request.getRequestDispatcher("/result.jsp").forward(request, response);
 	}
 
 }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -13,22 +13,21 @@
 		1. servlet에서 전달받은 데이터가 존재하는 경우와 , 존재하지 않는 경우를
 		나눠서 작업할것.
 		2. 작업시에는 core라이브러의 태그들을 사용할 것
+			 C:FOREACH 사용하여 하기
 	 -->
 	<div>
 		<!-- 일치하는 회원이 존재하지 않는 경우 -->
+		<c:if test="${empty resultList }">
 		<p style="color:red;">해당 이름의 회원이 존재하지 않습니다.</p>
-		
+		</c:if>
 		<!-- "성"을 검색한 경우 회원정보 출력예시 -->
-		<div class="member">
-            <p>이름: 심은성</p>
-            <p>나이: 4</p>
-            <p>성별: 남자</p>
-		</div>	
-		<div class="member">
-            <p>이름: 유상성</p>
-            <p>나이: 21</p>
-            <p>성별: 남자</p>
-		</div>	
+		<c:if test="${not empty resultList }">
+			<c:forEach var="find" items="${resultList}">
+			<p>이름 : ${find.name }</p>
+			<p>나이 : ${find.age }</p>
+			<p>성별 : ${find.gender }</p>
+			</c:forEach>
+		</c:if>
 	</div>   
 </body>
 </html>
